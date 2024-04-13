a07627651fed55565c06887337fbc2c013b8cec1
Akash Meheta <akashmeheta11@gmail.com>
2024-04-13 21:07:18 +0530

    Add Some ANIME Defualt Post for you

--- src/store/postStore.jsx
+++ src/store/postStore.jsx
@@ -60,19 +60,36 @@ const PostListProvider = ({ children }) => {
 const obj = [
     {
       id: 1,
-      title: "First Post",
-      content: "This is the content of the first post.",
-      author: "John Doe",
-      tags: ['post', 'first'],
-      img: "src/assets/react.svg",
+      title: "Trafalger. D Water Law",
+      content: "He posses the OP OP NOMI and a brilliant Doctor",
+      author: "Akash Meheta",
+      tags: ['One_Piece', 'OP'],
+      img: "src/assets/Screenshot 2024-02-16 200152.png",
     },
     {
       id: 2,
-      title: "Second Post",
-      content: "This is the content of the second post.",
-      author: "Jane Smith",
-      tags: ['post', 'first'],
+      title: "Ronoa Zoro",
+      content: "The Right hand Man Of The Emperor Luffy and a Man of Loyalty",
+      author: "Akash Meheta",
+      tags: ['3_Sword_Style', 'King_of_Hell'],
+      img: "src/assets/Screenshot 2023-11-03 221918.jpg",
     },
+    {
+        id: 3,
+        title: "Monkey. D Luffy",
+        content: "Who Posses the Isto Isto Nomi Model NIKA the Sun God",
+        author: "Akash Meheta",
+        tags: ['One_Piece', 'Gumo_Gumo'],
+        img: "src/assets/Screenshot 2023-11-07 190502 (1).jpg",
+      },
+      {
+        id: 4,
+        title: "Gojo Saturo",
+        content: "The Man With SIX Eye and power of Infinity",
+        author: "Akash Meheta",
+        tags: ['Jujutshu_Kaisan', 'Domain_Expansion', 'Infinite_Void'],
+        img: "src/assets/Screenshot 2023-11-17 100824.png",
+      },
 ];
 
 export default PostListProvider;
